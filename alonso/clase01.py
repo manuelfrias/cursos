6ec6fc505f598f81a6c5707acacf686af8b3fc3c
manuelfrias <63211732+manuelfrias@users.noreply.github.com>
2022-03-30 14:27:22 -0500

    Primer commit

---
+++ alonso/clase01.py
@@ -0,0 +1,17 @@
+# alonso = 18
+# edad = 0
+
+# for x in range(5):
+#     edad = edad + 1
+#     print(alonso + edad)
+
+# # alumnos = [20, 25, 23, 24, 26]
+
+# # for fgh in alumnos:
+# #     print(fgh)
+
+
+print(round(3.5))
+print(round(4.5))
+print(round(5.5))
+print(round(80.5))
